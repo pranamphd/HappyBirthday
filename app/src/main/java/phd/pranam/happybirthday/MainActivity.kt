fc80fc7cbc4954634da60451afae4d92c11fdb53
Pranam <pranam@pranam.phd>
2025-08-30 09:12:59 +0530

    change font size

--- app/src/main/java/phd/pranam/happybirthday/MainActivity.kt
+++ app/src/main/java/phd/pranam/happybirthday/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import phd.pranam.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +30,7 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun GreetingText(modifier: Modifier = Modifier, message: String) {
-    Text(text = message)
+    Text(text = message, fontSize = 100.sp, lineHeight = 116.sp)
 }
 @Preview(showBackground = true)
 @Composable
